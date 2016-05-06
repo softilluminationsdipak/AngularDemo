32d882dd328c27aeb727de9cee8b331ee4e58d24
Dipak Panchal <11dipak.contact@gmail.com>
2016-05-06 10:12:18 +0530

    assets precompiled locally

--- app/assets/javascripts/controllers/about_controller.js
+++ app/assets/javascripts/controllers/about_controller.js
@@ -1,13 +1,9 @@
 angular.module('demoAngular')
-.controller('AboutController', function ($scope, $rootScope, UserService, $location){
-  
-  console.log('-----1-----------'+$rootScope.isAuthenticated)
-  console.log('-----2-----------'+UserService.isAuthenticated())
+.controller('AboutController', function ($scope, $rootScope, UserService, $location){  
   if( $rootScope.isAuthenticated ){
   	$scope.name = "About Content";
   }else{
   	$location.path('/login')
   }
   
-
 });
