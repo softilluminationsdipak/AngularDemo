09af0499fbdad4bbb6395097e45747378498f542
Dipak Panchal <11dipak.contact@gmail.com>
2016-05-15 21:19:28 +0530

    Update configs

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module AngularDemo
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
