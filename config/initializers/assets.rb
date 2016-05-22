0c317cbea688ed5d4d3169377a645df284d93512
Dipak Panchal <11dipak.contact@gmail.com>
2016-05-22 17:29:53 +0530

    update assets for precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( application.js application.css common.js frontend.js frontend.css login.js login.css frontend_common.js dashboard.js dashboard.css)
+#Rails.application.config.assets.precompile += %w( application.js application.css common.js frontend.js frontend.css login.js login.css frontend_common.js dashboard.js dashboard.css)
+Rails.application.config.assets.precompile += %w( common.js)
